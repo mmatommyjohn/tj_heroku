c5f6f09fe1f06fdf923476aa1abc673385ec5eb2
Muhammad Alam <muhammad@tommyjohn.com>
2018-02-13 15:49:01 -0500

    re-order sample-route

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, "build")));
 app.get("/sample_route", (req, res) => {
   res.sendFile(path.join(__dirname, "sample_pages", "sample_page.html"));
 })
+app.use(express.static(path.join(__dirname, "build")));
 
 app.listen(port, () => console.log("Example app listening on port ", port));
