5520b3c320ffd8cecc2a6e894ad3c053208bece2
Muhammad Alam <muhammad@tommyjohn.com>
2018-02-22 14:32:26 -0500

    fix client/server routing conflict

--- server.js
+++ server.js
@@ -3,9 +3,14 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/sample_route", (req, res) => {
-  res.sendFile(path.join(__dirname, "sample_pages", "sample_page.html"));
-})
-app.use(express.static(path.join(__dirname, "build")));
+const buildPath = path.join(__dirname, "build");
+
+// app.get("/sample_route", (req, res) => {
+//   res.sendFile(path.join(__dirname, "sample_pages", "sample_page.html"));
+// })
+app.use(express.static(buildPath));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(buildPath, "index.html"));
+});
 
 app.listen(port, () => console.log("Example app listening on port ", port));
