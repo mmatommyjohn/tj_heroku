83bd61b89c60cbf8dca27041460baf347a6fb06b
Muhammad Alam <muhammad@tommyjohn.com>
2018-02-28 15:12:38 -0500

    change home and mark

---
+++ src/components/Mark.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Mark = () => (
+  <div className="mark">
+    <Link to="/home">
+      <svg className="home__icon" x="0px" y="0px" viewBox="0 0 68 90">
+        <path d="M 1,1 H 67 V 23 H 1 V 1 Z" className="horizontal_bar" />
+        <path d="M 1,45 H 23 V 89 H 1 V 45 Z" className="vertical_bar_left" />
+        <path
+          d="M 45,45 H 67 V 89 H 45 V 45 Z"
+          className="vertical_bar_right"
+        />
+      </svg>
+    </Link>
+  </div>
+);
+export default Mark;
--- src/routers/Router.js
+++ src/routers/Router.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
 
 import Header from "../components/Header";
+import Mark from "../components/Mark";
 import Home from "../components/Home";
 import NotFound from "../components/NotFound";
 import Category from "../components/Category";
@@ -14,7 +15,7 @@ const Router = () => (
     <div>
       <Header />
       <Switch>
-        <Route path="/" component={Home} exact={true} />
+        <Route path="/" component={Mark} exact={true} />
         <Route path="/home" component={Home} exact={true} />
         <Route path="/category" component={Category} />
         <Route path="/product" exact component={Product} />
