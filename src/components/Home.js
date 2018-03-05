64dafe934af882385bd05043ccda9ffd1898ad88
Muhammad Alam <muhammad@tommyjohn.com>
2018-03-05 15:19:59 -0500

    remove unsuccessful DY script inserting code from Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,6 @@
 import React from "react";
 
 class Home extends React.Component {
-  componentDidMount() {
-    const DYscript1 = document.createElement("script");
-
-    DYscript1.src = "//cdn.dynamicyield.com/api/8769373/api_dynamic.js";
-    DYscript1.async = true;
-    const DYscript2 = document.createElement("script");
-
-    DYscript2.src = "//cdn.dynamicyield.com/api/8769373/api_static.js";
-    DYscript2.async = true;
-
-    document.body.appendChild(DYscript1);
-    document.body.appendChild(DYscript2);
-  }
   render() {
     return (
       <div>
