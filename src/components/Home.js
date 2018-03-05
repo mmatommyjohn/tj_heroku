5ed46c2bdefe95ad4990a480c33cb96151810e10
Muhammad Alam <muhammad@tommyjohn.com>
2018-03-05 15:06:24 -0500

    update home component to include scripts for DY in lifecycle

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,27 @@
 import React from "react";
 
-const Home = () => (
-  <div>
-    <div id="banner" />
-    <h1>Home</h1>
-  </div>
-);
+class Home extends React.Component {
+  componentDidMount() {
+    const DYscript1 = document.createElement("script");
+
+    script.src = "//cdn.dynamicyield.com/api/8769373/api_dynamic.js";
+    script.async = true;
+    const DYscript2 = document.createElement("script");
+
+    script.src = "//cdn.dynamicyield.com/api/8769373/api_static.js";
+    script.async = true;
+
+    document.body.appendChild(DYscript1);
+    document.body.appendChild(DYscript2);
+  }
+  render() {
+    return (
+      <div>
+        <div id="banner" />
+        <h1>Home</h1>
+      </div>
+    );
+  }
+}
 
 export default Home;
