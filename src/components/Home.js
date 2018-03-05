6656025bb49355b9890899ce01e45e20ac9493da
Muhammad Alam <muhammad@tommyjohn.com>
2018-03-05 15:57:23 -0500

    add DY test adder take 2

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,18 @@
 import React from "react";
 
 class Home extends React.Component {
+  componentDidMount() {
+    window.DYO.smartObject("Dynamic Banner Adder", {
+      target: "dy_Dynamic_Banner_Adder",
+      inline: true
+    });
+  }
   render() {
     return (
       <div>
-        <div id="banner" />
+        <div id="banner">
+          <div id="dy_Dynamic_Banner_Adder" />
+        </div>
         <h1>Home</h1>
       </div>
     );
